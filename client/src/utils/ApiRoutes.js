6a65a4186cfe962bf39a01a567fee926153890f2
Himanshu Maurya <123646172+Himanshuatul@users.noreply.github.com>
2025-05-10 02:13:26 +0530

    Update ApiRoutes.js

--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -1,4 +1,4 @@
-export const HOST ="https://whatsapp-clone-10.onrender.com";
+export const HOST ="https://whatsapp-backend-zral.onrender.com";
 const AUTH_ROUTE =`${HOST}/api/auth`;
 const MESSAGES_ROUTE=`${HOST}/api/messages`;
 export const CHECK_USER_ROUTE =`${AUTH_ROUTE}/check-user`;
