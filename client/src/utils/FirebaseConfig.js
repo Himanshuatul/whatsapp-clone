c353bc167f11f4a75c2d6cb299da424d598d58ce
Himanshu Maurya <123646172+Himanshuatul@users.noreply.github.com>
2025-05-10 15:32:20 +0530

    Update FirebaseConfig.js

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAyLcTSEMkZb2J6tGk9EmP6c2R_ixaTonA",
-  authDomain: "whatsapp-clone-a9080.firebaseapp.com",
-  projectId: "whatsapp-clone-a9080",
-  storageBucket: "whatsapp-clone-a9080.firebasestorage.app",
-  messagingSenderId: "720067480978",
-  appId: "1:720067480978:web:6515b9a2420c39031a454b",
-  measurementId: "G-DDG8HZMMDT"
+  apiKey: "AIzaSyAjzibl-_CFVJb48ITQyXSTeNVsJBY7Gpk", // from your Firebase Console
+  authDomain: "whatsapp-clone-frontend-1acdf.firebaseapp.com",
+  projectId: "whatsapp-clone-frontend-1acdf",
+  storageBucket: "whatsapp-clone-frontend-1acdf.appspot.com",
+  messagingSenderId: "305854813652",
+  
 };
 
 const app = initializeApp(firebaseConfig);
-export const firebaseAuth =getAuth(app)
+export const firebaseAuth = getAuth(app);
