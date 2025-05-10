bd476111c6621e9a09476845e35b57bb53d52ba3
Himanshu Maurya <123646172+Himanshuatul@users.noreply.github.com>
2025-05-10 15:41:38 +0530

    Update FirebaseConfig.js

--- client/src/utils/FirebaseConfig.js
+++ client/src/utils/FirebaseConfig.js
@@ -1,14 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAjzibl-_CFVJb48ITQyXSTeNVsJBY7Gpk", // from your Firebase Console
+  apiKey: "AIzaSyAJzibI-_CFVJb48lTQyXSTeNVsJBY7Gpk",
   authDomain: "whatsapp-clone-frontend-1acdf.firebaseapp.com",
   projectId: "whatsapp-clone-frontend-1acdf",
-  storageBucket: "whatsapp-clone-frontend-1acdf.appspot.com",
+  storageBucket: "whatsapp-clone-frontend-1acdf.firebasestorage.app",
   messagingSenderId: "305854813652",
-  
+  appId: "1:305854813652:web:237f84d8d38b6e9afcf241",
+  measurementId: "G-JFTK7YHHTZ"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const firebaseAuth = getAuth(app);
+const analytics = getAnalytics(app);
